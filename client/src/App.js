b35e9f6c3ce9c0fd05ccbbfa5644d0681e5ea95b
Bibek Chaudhary <kingbibekc@gmail.com>
2021-10-17 20:08:15 +0545

    added react router dom and react router bootstrap and styled product and products page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Route } from "react-router";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => (
   <div>
     <Header />
     <main className="py-3">
       <Container>
-        <h1>Welcome to my shop</h1>
+        <Route exact path="/" component={HomeScreen} />
+        <Route path="/product/:id" component={ProductScreen} />
       </Container>
     </main>
     <Footer />
